f2301d69b1c31ac25a2331113a7ed46a9b8dbd56
Ilmira35 <abdullayevailmira6@gmail.com>
2025-11-14 16:21:31 +0500

    O'rgandim

--- php.php
+++ php.php
@@ -540,23 +540,30 @@
 //         echo $item['Ism'].", ";
 // } 
 
-                
+             // O'n beshinchi dars
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+date_default_timezone_set("Asia/Tashkent");
+echo "Xozirgi sana:". date("Y-m-d")."<br>";
+echo "Xozirgi sana:". date("Y m d")."<br>";
+echo "Xozirgi sana:". date("Y.m.d")."<br>";
+echo "Xozirgi sana:". date("d.m.Y")."<br>";
+echo "Xozirgi sana:". date("d.m.y")."<br>";
+echo "Xozirgi sana:". date("D.m.Y")."<br>";
+echo "Xozirgi sana:". date("D.M.Y")."<br>";
+echo "Xozirgi vaqt:". date("H:i:s")."<br>";
+echo "Xozirgi vaqt:". date("h:i:sa")."<br>";
+echo "Xozirgi vaqt:". date("l")."<br>";
+echo "Xozirgi sana va vaqt:". date("H:i d.m.Y")."<br>";
+echo "Xozirgi sana va vaqt:". date("d.m.Y H:i")."<br>";
+echo "Xozirgi sana:". date("d F Y")."<br>";
+// Unix time
+echo "Unix time: ".time()."<br>";
+echo "Unix time: ".strtotime("2025-11-14")."<br>";
+echo "Unix time: ".date("d.m.Y",strtotime("2025-11-14"))."<br>";
+echo "Unix time: ".date("d.m.Y", strtotime("+7 month"))."<br>";
+echo "Unix time: ".date("d.m.Y",time())."<br>";
+          
 
 
 ?>
